12f94df9872c2eff3c3c6470117c9c9ef56c3d86
arham09 <arham.abiyan@gmail.com>
2019-04-01 17:28:21 +0700

    Add Password Validator

--- app/helper/misc.js
+++ app/helper/misc.js
@@ -46,5 +46,23 @@ module.exports = {
     }
 
     return res.status(resultPrint.status).json(resultPrint)
+  },
+  setPassword: (password, salt) => {
+    const crypto = require('crypto')
+    let hash = crypto.createHmac('sha512', salt)
+    hash.update(password)
+    let value = hash.digest('hex')
+    return {
+      salt: salt,
+      passwordHash: value
+    }
+  },
+  generateSalt: (length) => {
+    const crypto = require('crypto')
+    return crypto.randomBytes(Math.ceil(length / 2)).toString('hex').slice(0, length)
+  },
+  validatePassword: (password) => {
+    var patt = /^(?=.*?[A-Z])(?=(.*[a-z]){1,})(?=(.*[\d]){1,})(?=(.*[\W_]){1,})(?!.*\s).{6,15}$/
+    return patt.test(password)
   }
 }
