e1d8ae5de415f5c21230c972c70c8540f1510769
arham09 <arham.abiyan@gmail.com>
2019-04-06 21:16:10 +0700

    Fix routes users

--- app/routes/v1/users.js
+++ app/routes/v1/users.js
@@ -1,10 +1,11 @@
-/* global UsersControllers */
+/* global UsersControllers AuthHelper */
 
 'use strict'
 
 var Route = express.Router()
 
 Route
+  .all('/*', AuthHelper.requiresAuthorization)
   .post('/register', UsersControllers.register)
   .post('/login', UsersControllers.login)
 
